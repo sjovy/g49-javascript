acde2d09accc3bf304de60b9d6ce4a80005dedf2
Negar Baharmand <negarbaharmand@outlook.com>
2024-07-08 11:11:48 +0200

    Functions, string, date

--- js-fundementals.js
+++ js-fundementals.js
@@ -69,3 +69,108 @@ let isEqual = n1 == n2; //true
 console.log(isEqual);
 
 let isStrictEqual = n1 === n2; //false
+
+//1.Function Declaration
+function greetDeclaration(name) {
+  console.log("Hello " + name + "!");
+}
+greetDeclaration("Negar");
+
+function addition(n1, n2) {
+  return n1 + n2;
+}
+console.log(addition(10, 20));
+
+//2. Function Expression
+const greetExpression = function (name) {
+  console.log("Hello " + name + "!");
+};
+greetExpression("Elnaz");
+
+//3. Arrow Function
+const greetArrow = (name) => console.log("Hello " + name + "!");
+
+greetArrow("JS");
+
+const _addition = (n1, n2) => {
+  const result = n1 + n2;
+  return result;
+};
+console.log(_addition(10, 30));
+
+function sum(...numbers) {
+  // let total = 0;
+  // for (let number of numbers) {
+  //   total += number;
+  // }
+  let total = 0;
+  numbers.forEach((number) => {
+    total += number;
+  });
+  return total;
+}
+console.log(sum(1, 2, 3, 4, 5)); //15
+
+let _name = "Alice";
+let _age = 21;
+
+let _message =
+  "Hello, my name is " + _name + " and I am " + _age + " years old.";
+console.log(_message);
+
+let templateMessage =
+  //template literal (backtick `)
+  `Hello, my name is ${_name} and I am ${_age} years old.`;
+console.log(templateMessage);
+
+const g = 5;
+const h = 10;
+const theResult = `The sum of g and h is ${g + h}`;
+console.log(theResult);
+
+function add(x = 0, y = 0) {
+  return x + y;
+}
+function subtraction(x, y) {
+  return x - y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if (y === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return x / y;
+}
+
+function calculate(num1, num2, operation) {
+  return operation(num1, num2);
+}
+
+console.log(calculate(10, 0, subtraction));
+
+try {
+  console.log(calculate(10, 0, divide));
+} catch (error) {
+  console.log("Caught an error: ", error.message);
+} finally {
+  console.log("Finally block has been executed");
+}
+
+let text = "HEllo, World!";
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.indexOf("World"));
+console.log(text.slice(0, 5));
+
+//Date
+const currentDate = new Date(); //current date
+console.log(currentDate);
+console.log(currentDate.getFullYear());
+console.log(currentDate.getMonth() + 1);
+console.log(currentDate.getSeconds());
+
+// console.log(currentDate.toLocaleDateString());
+console.log(currentDate.toLocaleDateString("sv-SE"));
